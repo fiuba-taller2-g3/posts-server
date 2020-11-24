42568206ed92d6b530011b74576f573aba8bc876
Patricio Avigliano <patricioavigliano@gmail.com>
2020-11-24 03:00:12 +0100

    5 first posts endpoints

--- app.py
+++ app.py
@@ -1,25 +1,60 @@
-from flask import Flask, request
-from init_db import *
+import json
+
+from datetime import datetime
+from flask import Flask, request, make_response, jsonify
+from db_service import *
 
 app = Flask(__name__)
 
+conn = connect()
+set_db(conn, INIT_CMD)
 
 @app.route('/')
 def hello():
     response = 'db: UP\n'
-    conn = connect()
     try:
-        use_db(conn,'SELECT NOW();')
+        use_db(conn,'\SELECT NOW();')
     except Exception as e:
         response = "Error {}: {}\n".format(type(e).__name__, e.args)
-    finally:
-        disconnect(conn)
-
     return response
 
+@app.route('/posts/reset', methods=['DELETE'])
+def reset_posts():
+    set_db(conn, RESET_CMD)
+    return make_response("DB Reseted", 200)
+
+@app.route('/posts', methods=['POST'])
+def new_post():
+    body = request.json
+    post_id, = use_db(conn, add_post_query(body['user_id'], body['price'], body['date']))
+    return make_response(jsonify(id=post_id), 201)
+
+@app.route('/posts/<post_id>')
+def visualize_post(post_id):
+    user_id, price, date = use_db(conn, get_post_query(post_id))
+    return make_response(jsonify(user_id=user_id, date=date.strftime('%Y-%m-%d') , price=price), 200)
+
+@app.route('/posts/<post_id>', methods=['PATCH'])
+def edit_post(post_id):
+    return use_db(conn, )
+
+@app.route('/posts/<post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    id, = use_db(conn, delete_post_query(post_id))
+    return make_response("Post {} deleted successfully".format(id), 200)
+
+@app.route('/posts')
+def visualize_posts_from_user():
+    user_id = request.args.get('user_id')
+    posts = use_db(conn, get_posts_query(user_id))
+    parsed_posts = []
+    for id, user_id, price, date, is_blocked in posts:
+        parsed_posts.append({"id":id, "user_id":user_id, "price":price, "date":date.strftime('%Y-%m-%d'), "is_blocked":is_blocked})
+    return make_response(jsonify(parsed_posts), 200)
 
 if __name__ == '__main__':
     try:
         app.run(port=os.environ['PORT'])
     except KeyError:
         app.run()
+    disconnect(conn)
---
+++ db_service.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+import psycopg2
+
+
+CREATE_POSTS_TABLE_CMD = "\
+                CREATE TABLE IF NOT EXISTS posts (\
+                    id SERIAL PRIMARY KEY,\
+                    user_id INT NOT NULL,\
+                    price INT NOT NULL,\
+                    date DATE NOT NULL,\
+                    is_blocked BOOLEAN DEFAULT false\
+                );\
+                "
+
+INIT_CMD = CREATE_POSTS_TABLE_CMD
+
+def add_post_query(user_id, price, date):
+    return "\
+                INSERT INTO posts(user_id, price, date)\
+                VALUES ('{}', '{}', '{}')\
+                RETURNING id".format(user_id, price, date)
+
+def get_post_query(post_id):
+    return "\
+                SELECT user_id, price, date FROM posts WHERE id = '{}'".format(post_id)
+
+def get_posts_query(user_id):
+    return "\
+                SELECT * FROM posts WHERE user_id = '{}'".format(user_id)
+
+def delete_post_query(post_id):
+    return "\
+                DELETE FROM posts\
+                WHERE id = '{}'\
+                RETURNING id".format(post_id)
+
+DROP_ALL_CMD = "\
+                DROP SCHEMA public CASCADE;\
+                CREATE SCHEMA public;\
+                GRANT ALL ON SCHEMA public TO postgres;\
+                GRANT ALL ON SCHEMA public TO public;\
+                "
+
+RESET_CMD = DROP_ALL_CMD + INIT_CMD
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+            dbname = "postgres",
+            user = "postgres",
+            host = "psql-container",
+            password = "postgres"
+        )
+
+        # create a cursor
+        cur = conn.cursor()
+
+	    # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return conn
+
+def disconnect(conn):
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+
+def set_db(conn, command):
+    with conn.cursor() as cursor:
+        cursor.execute(command)
+        conn.commit()
+
+def use_db(conn, command):
+    with conn.cursor() as cursor:
+        cursor.execute(command)
+        conn.commit()
+        results = cursor.fetchall()
+    # print(results)
+    if len(results) == 1:
+        return results[0]
+    return results
